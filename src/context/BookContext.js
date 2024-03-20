7f4ca046b157336c67b8bb3d34fec823a9c09110
Agafina <98034773+Agafina@users.noreply.github.com>
2024-03-20 19:12:53 +0100

    use Reducer to help dispatch the functions easily

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,24 +1,14 @@
-import React, { createContext, useState } from 'react';
-import {v4 as uuidv4} from 'uuid'
+import React, { createContext, useReducer, useState } from 'react';
+import { BookReducer } from '../reducers/BookReducer';
+
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: 'The time to say Goodbye', author: 'Brain yu', id:uuidv4() },
-        {title: 'Domain expansion', author: 'Silivain jus', id:uuidv4() },
-        {title: 'Great Warrior', author: 'Agatha prime', id:uuidv4() },
-        {title: 'Looking for Luck', author: 'Stefen gain', id:uuidv4() },
-    ])
+    const [books,  dispatch ] = useReducer(BookReducer ,[])
 
-    function addBook (title, author){
-        setBooks([...books, {title, author}])
-    }
-    function removeBook (id){
-        setBooks(books.filter(book => book.id !== id))
-    }
     return ( 
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             {props.children}
         </BookContext.Provider>
      );
