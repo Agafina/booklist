32f8e6f989c37e51bcfe1e630c33cf226960c45b
Agafina <98034773+Agafina@users.noreply.github.com>
2024-03-20 12:49:47 +0100

    Used Forms to add book dynamically

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,6 +1,23 @@
+import { useContext, useState } from "react";
+import { BookContext } from "../context/BookContext";
+
 const BookForm = () => {
+    const {addBook} = useContext(BookContext)
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addBook(title, author)
+        setTitle('')
+        setAuthor('')
+    }
     return ( 
-        <div></div>
+        <form className="book-form" onSubmit={handleSubmit}>
+            <input type="text" required placeholder="Enter Title of Book" value={title} onChange={(e) => setTitle(e.target.value)}  />
+            <input type="text" required placeholder="Enter Book's Author" value={author} onChange={(e) => setAuthor(e.target.value)}  />
+            <input type="submit" value='add book' />
+        </form>
      );
 }
  
